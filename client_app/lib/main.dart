c69596d7a6ebe4f251f626c3674d858bfaa32eb3
Jackk-Doe <dodo-stv@hotmail.com>
2022-10-15 22:47:42 +1300

    Update : changed MyApp state type in main.dart
    - changed MyApp class to StatefulWidget

--- client_app/lib/main.dart
+++ client_app/lib/main.dart
@@ -6,13 +6,18 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Basic AUTH with Flutter + FastAPI',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
