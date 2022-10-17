a2e593e312be9d1a32112f2469a62f192579e8cc
Jackk-Doe <dodo-stv@hotmail.com>
2022-10-17 23:14:45 +1300

    Update : SignIn screen page UI

--- client_app/lib/screens/signin_screen.dart
+++ client_app/lib/screens/signin_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:client_app/screens/screens.dart';
+import 'package:client_app/widgets/widgets.dart';
+
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
 
@@ -8,10 +11,63 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+
+  void _signIn() {
+    // TODO : SignIn function
+  }
+
+  void _changeToSignUp() {
+    Navigator.pushReplacement(
+      context, 
+      MaterialPageRoute(builder: (context) => const SignUpPage()),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('This is a SignIn page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Sign-In into user account", style: TextStyle(fontSize: 28)),
+            const SizedBox(height: 40),
+
+            CustomTextField(controller: _emailController, hintText: "User email"),
+            const SizedBox(height: 15),
+
+            CustomTextField(controller: _passwordController, hintText: "User password"),
+            const SizedBox(height: 35),
+
+            CustomElevatedButton(onPressfunc: _signIn, buttonText: 'Sign In'),
+            const SizedBox(height: 10),
+
+            TextButton(
+              onPressed: _changeToSignUp,
+              child: const Text(
+                'go to Create user accout',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
