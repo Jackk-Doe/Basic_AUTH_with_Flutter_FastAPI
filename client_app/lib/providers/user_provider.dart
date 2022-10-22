e4bc0192664e8b66ed61c10cef305154150da10d
Jackk-Doe <dodo-stv@hotmail.com>
2022-10-22 22:05:57 +1300

    Update : UserProvider file : made [_user] nullable

--- client_app/lib/providers/user_provider.dart
+++ client_app/lib/providers/user_provider.dart
@@ -3,14 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:client_app/models/models.dart';
 
 class UserProvider extends ChangeNotifier {
-  
-  User _user = User(
-    username: '',
-    email: '',
-    token: '',
-  );
 
-  User get user => _user;
+  //* If [_user] is null, if not Signed-In
+  User? _user;
+
+  // UserProvider() {
+  //   _user = User(
+  //     username: '',
+  //     email: '',
+  //     token: '',
+  //   );
+  // }
+
+  User? get user => _user;
 
   void setUserFromJson(String user) {
     // NOTE : JSON format, in dart is actually String data type
