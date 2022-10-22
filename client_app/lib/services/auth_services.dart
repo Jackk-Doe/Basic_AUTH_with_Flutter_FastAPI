899073103c542ad655eddbaf90a226c40813824e
Jackk-Doe <dodo-stv@hotmail.com>
2022-10-23 01:20:38 +1300

    Update : added signInUser() AuthService

--- client_app/lib/services/auth_services.dart
+++ client_app/lib/services/auth_services.dart
@@ -29,15 +29,44 @@ class AuthService {
       );
 
       bool hasError = ErrorHandling.httpErrorHandling(response: res, context: context);
-
-      if (hasError) {
-        /// Has HTTP Error
-        return null;
-      }
+      /// Has HTTP Error
+      if (hasError) return null;
 
       /// Execute successfully
       return User.fromJson(res.body);
 
+    } catch (e) {
+      Utils.showSnackBar(context, e.toString());
+      return null;
+    }
+  }
+
+
+  /// A function for Sign-Up user account,
+  /// Success : return User model,
+  /// Fail : return null
+  static Future<User?> signInUser({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserAuth userAuth = UserAuth(email, password);
+
+      http.Response res = await http.post(
+        Uri.parse("${Constants.URI}/users/signin"),
+        body: userAuth.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+      );
+
+      bool hasError = ErrorHandling.httpErrorHandling(response: res, context: context);
+      if (hasError) return null;
+
+      /// Execute successfully
+      return User.fromJson(res.body);
+      
     } catch (e) {
       Utils.showSnackBar(context, e.toString());
       return null;
