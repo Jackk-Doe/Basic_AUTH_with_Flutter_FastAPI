fd0f69b49585e3a65149b57e5aaac58c1ff56715
Jackk-Doe <dodo-stv@hotmail.com>
2022-10-23 16:36:26 +1300

    Update : system to remember in-device datas
    - check if token existed in device.
    - if existed, make a call to backend.
    - Go to Home page, instead of SignUp page

--- client_app/lib/main.dart
+++ client_app/lib/main.dart
@@ -3,9 +3,16 @@ import 'package:provider/provider.dart';
 
 import 'package:client_app/screens/screens.dart';
 import 'package:client_app/providers/providers.dart';
+import 'package:client_app/models/models.dart';
+import 'package:client_app/services/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -16,25 +23,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  /// In the widget Initialising process, if [token] is found in-divice
+  /// get User data from backend, then notify UserProvider
+  void _getUserData() async {
+    String? existedToken = await LocalStoreServices.getFromLocal(context);
+    if (existedToken != null) {
+      User? user = await AuthService.getUser(context: context, token: existedToken);
+      if (user != null) {
+        if (!mounted) return null;
+        Provider.of<UserProvider>(context, listen: false).setUserFromModel(user);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => UserProvider(),
-      child: MaterialApp(
-        title: 'Basic AUTH with Flutter + FastAPI',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Consumer<UserProvider>(
-          builder: (context, userProvider, child) {
+    return MaterialApp(
+      title: 'Basic AUTH with Flutter + FastAPI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Consumer<UserProvider>(
+        builder: (context, userProvider, child) {
+          if (userProvider.user != null) {
+            return const HomePage();
+          }
 
-            if (userProvider.user != null) {
-              return const HomePage();
-            }
-
-            return const SignUpPage();
-          },
-        ),
+          return const SignUpPage();
+        },
       ),
     );
   }
